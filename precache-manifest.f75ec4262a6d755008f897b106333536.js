b0a74d2c87662026024e904b46523ed0240eefc5
Anastasia Romanova <Anastasiachaga@yandex.ru>
2023-03-12 04:04:33 +0300

    Updates

---
+++ precache-manifest.f75ec4262a6d755008f897b106333536.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = [
+  {
+    "revision": "18baa1d47a1fe4062ac1",
+    "url": "/mesto-react/static/css/main.f8e318c7.chunk.css"
+  },
+  {
+    "revision": "18baa1d47a1fe4062ac1",
+    "url": "/mesto-react/static/js/main.ecb40128.chunk.js"
+  },
+  {
+    "revision": "f07ee47d86a4e858ab90",
+    "url": "/mesto-react/static/js/runtime~main.9304fbdd.js"
+  },
+  {
+    "revision": "de7fc80cf1a9c785079c",
+    "url": "/mesto-react/static/js/2.5b4a441a.chunk.js"
+  },
+  {
+    "revision": "c563299317d52c1696cd",
+    "url": "/mesto-react/static/js/3.937f6cc8.chunk.js"
+  },
+  {
+    "revision": "7baba3c1428ee943536a8022d42f0985",
+    "url": "/mesto-react/static/media/Vector.7baba3c1.svg"
+  },
+  {
+    "revision": "d22a0d6612cceed0724e681a3a1e90da",
+    "url": "/mesto-react/static/media/Inter-Regular.d22a0d66.woff2"
+  },
+  {
+    "revision": "0dae34d5a70aa5062a7dd26ae030780d",
+    "url": "/mesto-react/static/media/Inter-Bold.0dae34d5.woff2"
+  },
+  {
+    "revision": "30690bb6519a21e77ed29ff123497df3",
+    "url": "/mesto-react/static/media/Inter-Bold.30690bb6.woff"
+  },
+  {
+    "revision": "6bfdb35467f51d3ba1f70f8d4a9c2b59",
+    "url": "/mesto-react/static/media/Vector2.6bfdb354.svg"
+  },
+  {
+    "revision": "4d127124f941ebf54a749cf9ab10e545",
+    "url": "/mesto-react/static/media/Inter-Regular.4d127124.woff"
+  },
+  {
+    "revision": "49c59986c2a8f1a08a4002b17e2dc62d",
+    "url": "/mesto-react/static/media/Inter-Black.49c59986.woff2"
+  },
+  {
+    "revision": "b2fce4ac45f3d116cd48ec8d068ab723",
+    "url": "/mesto-react/static/media/Vector1.b2fce4ac.svg"
+  },
+  {
+    "revision": "af718f919aafb2f3178ac634c7f37082",
+    "url": "/mesto-react/static/media/Inter-Black.af718f91.woff"
+  },
+  {
+    "revision": "3d813f810a41032c140568c878fd7d85",
+    "url": "/mesto-react/static/media/Union.3d813f81.svg"
+  },
+  {
+    "revision": "4831282e649d143ce022040beb49ad4f",
+    "url": "/mesto-react/static/media/Trash.4831282e.svg"
+  },
+  {
+    "revision": "00b1c22a19c7a1e64f19b1fbacc3a301",
+    "url": "/mesto-react/static/media/like.00b1c22a.svg"
+  },
+  {
+    "revision": "d476a8b86d0b656cfa463c1148e96069",
+    "url": "/mesto-react/static/media/Vector3.d476a8b8.svg"
+  },
+  {
+    "revision": "f09ebada5030d2c9090ee99a87b3f86c",
+    "url": "/mesto-react/index.html"
+  }
+];
